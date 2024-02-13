a725233f72f1158b1c6bfe8960c874917eca44bb
dtarko2020 <dtarko2020@yula.org>
2024-02-13 15:31:20 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} /> {/* Use 'element' to define components */}
 
